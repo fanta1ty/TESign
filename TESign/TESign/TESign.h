94a753cc10c53a8961e67758e503400b4aa21f3a
Thinh Nguyen <thinhnguyen12389@gmail.com>
2020-03-21 17:08:15 +0700

    Initial project setup

---
+++ TESign/TESign/TESign.h
@@ -0,0 +1,19 @@
+//
+//  TESign.h
+//  TESign
+//
+//  Created by User on 3/21/20.
+//  Copyright © 2020 ThinhNguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TESign.
+FOUNDATION_EXPORT double TESignVersionNumber;
+
+//! Project version string for TESign.
+FOUNDATION_EXPORT const unsigned char TESignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TESign/PublicHeader.h>
+
+
